731c34183820f90301c2f75565c8370926e7e2d6
Pixelinkler <83833539+Pixelinkler@users.noreply.github.com>
2024-03-18 22:01:22 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,3 @@
 bhagwan_naam = [
-    "Raam Raam", "Shiv Shiv", "Radhe Radhe", "Narayan", "Namaskaram",
-    "Vanakam", "Jai Shri Ram", "Jai Jagannath", "Jai Hari Vittal",
-    "Shri Swami Samartha", "Shiv Hari Shiv Hari", "Har Har Mahadev!", "Jai Jai Raguveer Samartha!"
+    "Raam Raam", "Shiv Shiv", "Radhe Radhe", "Narayan", "Namaskaram", "Vanakam"
 ]
